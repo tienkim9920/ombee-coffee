7abdf87fb1ce2844fc6b81eb0c7b047ae55040fe
Tien Kim <tienkim9920@gmail.com>
2022-07-09 15:39:59 +0700

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ombee-coffee" 
---
+++ logic/shareScript.js
@@ -0,0 +1,5 @@
+function loadScript(component, id) {
+  fetch(component)
+    .then((res) => res.text())
+    .then((text) => (document.getElementById(id).innerHTML = text));
+}
